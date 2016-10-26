08d56b2033b160dac59299f24236bf2f23d49937
Denis Azhymov <azhimov93@gmail.com>
2016-10-26 14:01:06 +0300

    Initial commit

---
+++ src/FifthClass.java
@@ -0,0 +1,6 @@
+
+public class FifthClass {
+    public static void main(String[] args) {
+        System.out.print("Мой пятый класс");
+    }
+}
---
+++ src/FouthClass.java
@@ -0,0 +1,7 @@
+
+public class FouthClass {
+    public static void main(String[] args) {
+
+        System.out.print("Мой четвертый класс");
+    }
+}
